95843f137a1c276d9dcf8fdd36dd5cba7bde2661
Furkan Küçük <furkankucuk146@gmail.com>
2024-12-16 00:15:13 +0300

    Json Web Token

---
+++ Project12_JwtToken/FrmLogin.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Project12_JwtToken
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(181, 172);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(168, 44);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Giriş Yap";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(62, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(181, 47);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(207, 28);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(181, 95);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(207, 28);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Şifre:";
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 269);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "FrmLogin";
+            this.Text = "Giriş Formu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Project12_JwtToken/JWT/TokenGenerator.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project12_JwtToken.JWT
+{
+    public class TokenGenerator
+    {
+        public string GenerateJwtToken(string username,string email,string name, string surname)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("20Derste20ProjeToken+-*/1234tokenJWT")); //token ın içine oluşturdugum imza
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256); //security key ve token olustururken kullanacagım şifreleme algoritmasını tutar
+            var claimsExample = new[]
+            {//tokenımın temel parametrelerini tutar
+                new Claim(JwtRegisteredClaimNames.Sub,username),
+                new Claim(JwtRegisteredClaimNames.Email,email),
+                new Claim(JwtRegisteredClaimNames.GivenName,name),
+                new Claim(JwtRegisteredClaimNames.FamilyName,surname),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "localhost",//token ın yayıncısı,kim tarafından olusturuldu token
+                audience: "localhost", //token ı kim dinliyor
+                claims: claimsExample, //token ın parametreleri nerden geliyor
+                expires: DateTime.Now.AddMinutes(5),//token ne zamana kadar geçerli, burada olusturuldutan 5 dk sonra kapanıyor
+                signingCredentials: credentials); //tokenın şifreleme algoritması gibi parametreleri neler
+
+            return new JwtSecurityTokenHandler().WriteToken(token); //token olusmus oldu
+        }
+
+        public string GenerateJwtToken2(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("20Derste20ProjeToken+-*/1234tokenJWT")); //token ın içine oluşturdugum imza
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256); //security key ve token olustururken kullanacagım şifreleme algoritmasını tutar
+            var claimsExample = new[]
+            {//tokenımın temel parametrelerini tutar
+                new Claim(JwtRegisteredClaimNames.Sub,username),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "localhost",//token ın yayıncısı,kim tarafından olusturuldu token
+                audience: "localhost", //token ı kim dinliyor
+                claims: claimsExample, //token ın parametreleri nerden geliyor
+                expires: DateTime.Now.AddMinutes(5),//token ne zamana kadar geçerli, burada olusturuldutan 5 dk sonra kapanıyor
+                signingCredentials: credentials); //tokenın şifreleme algoritması gibi parametreleri neler
+
+            return new JwtSecurityTokenHandler().WriteToken(token); //token olusmus oldu
+        }
+    }
+}
